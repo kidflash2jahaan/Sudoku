ae9439f1ff46f578415d271b13fc8bcf7ba4c6b8
Jahaan Pardhanani <kidflash2jahaan@gmail.com>
2024-12-30 10:51:36 +0530

    V1.1 Security Update

--- src/GlobalStateContext.jsx
+++ src/GlobalStateContext.jsx
@@ -1,33 +1,13 @@
-import {createContext, useEffect, useState} from 'react';
+import {createContext, useState} from 'react';
 
 export const GlobalStateContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export const GlobalStateProvider = ({children}) => {
-    const endpoint = "https://healthy-glider-liberal.ngrok-free.app/sudoku"
+    const endpoint = import.meta.env.VITE_ENDPOINT
     const [username, setUsername] = useState("");
 
-    useEffect(() => {
-        const originalFetch = window.fetch;
-        
-        window.fetch = async (url, options = {}) => {
-            const headers = {
-                "ngrok-skip-browser-warning": "true",
-                ...options.headers,
-            };
-            
-            return originalFetch(url, {
-                ...options,
-                headers,
-            });
-        };
-        
-        return () => {
-            window.fetch = originalFetch;
-        };
-    }, []);
-    
     return (<GlobalStateContext.Provider value={{endpoint, username, setUsername}}>
-            {children}
-        </GlobalStateContext.Provider>);
+        {children}
+    </GlobalStateContext.Provider>);
 };
